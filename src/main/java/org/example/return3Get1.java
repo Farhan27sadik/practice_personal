73aa7b5930cfb3db9a53aecd45e977d5ecb7922c
Mohammad Farhan Sadik <miriam.jmcd@gmail.com>
2023-04-14 09:26:39 +0600

    return3Get1.java updated

--- src/main/java/org/example/return3Get1.java
+++ src/main/java/org/example/return3Get1.java
@@ -1,21 +1,26 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class return3Get1 {
     public static void main(String[] args) {
-        int n;
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        System.out.println(totalBottleCount(n));
+        int n = sc.nextInt();
+        System.out.println(returnPepsirBotol.apply(n));
     }
-    static int totalBottleCount(int n){
-        int res;
-        if(n>=3){
-            res =n+ (n / 3);
-            return res;
-        }
 
-        return totalBottleCount(n%3);
-    }
+    static Function<Integer, Integer> returnPepsirBotol = pepsirBotol -> {
+        Integer output = pepsirBotol;
+        int initialOutput = pepsirBotol / 3;
+        if (initialOutput>=1)
+            output += initialOutput;
+        while (initialOutput >= 3) {
+            pepsirBotol = initialOutput;
+            initialOutput = pepsirBotol/3;
+            output += initialOutput;
+
+        }
+        return output;
+    };
 }
